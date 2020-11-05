634c7ce5898520353b4eacd6c959c74291234687
Dylan Besonen <besonend@gmail.com>
2020-11-05 11:51:00 -0500

    added grid to the footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,23 @@
+import { Grid } from '@material-ui/core';
 import React from 'react';
 
 
 function Footer() {
     return (
         <>
-            <footer style={{ textAlign: "center", backgroundColor: "grey"}}>
-                <a style={{ textAlign: "center", margin: "0", }}>LinkedIn</a>
+            <footer style={{ backgroundColor: "#4d9699" }}>
+                <Grid container>
+                    <Grid item xs={4} style={{ textAlign: "center" }}>
+                        <h3 style={{margin: "5px"}}>Social Media:</h3>
+                        <a href="https://www.linkedin.com/in/besonend/" style={{ textDecoration: "none", textAlign: "center", margin: "0", color: "black" }}>LinkedIn</a>
+                        <div>
+                            <a href="https://github.com/besonend" style={{ textDecoration: "none", textAlign: "center", margin: "0", color: "black" }}>GitHub</a>
+                        </div>
+                    </Grid>
+                    <Grid item xs={4} style={{ textAlign: "center" }}>
+                        <a style={{ textAlign: "center", margin: "0", }}>GitHub</a>
+                    </Grid>
+                </Grid>
             </footer>
         </>
     );
