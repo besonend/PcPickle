6750111dcd25ad654e32dd02c120c44a54b2068b
Dylan Besonen <besonend@gmail.com>
2020-11-05 11:54:30 -0500

    finished styling just need to add more links

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,15 +7,17 @@ function Footer() {
         <>
             <footer style={{ backgroundColor: "#4d9699" }}>
                 <Grid container>
+                    <Grid item xs={4}></Grid>
                     <Grid item xs={4} style={{ textAlign: "center" }}>
-                        <h3 style={{margin: "5px"}}>Social Media:</h3>
+                        <h3 style={{margin: "5px"}}>Creator Links:</h3>
                         <a href="https://www.linkedin.com/in/besonend/" style={{ textDecoration: "none", textAlign: "center", margin: "0", color: "black" }}>LinkedIn</a>
-                        <div>
+                        <div style={{marginBottom: "15px"}}>
                             <a href="https://github.com/besonend" style={{ textDecoration: "none", textAlign: "center", margin: "0", color: "black" }}>GitHub</a>
                         </div>
                     </Grid>
                     <Grid item xs={4} style={{ textAlign: "center" }}>
-                        <a style={{ textAlign: "center", margin: "0", }}>GitHub</a>
+                    <h3 style={{margin: "5px"}}>Repo:</h3>
+                        <a href="https://github.com/besonend/PcPickle" style={{ textDecoration: "none", textAlign: "center", margin: "0", color: "black" }}>GitHub</a>
                     </Grid>
                 </Grid>
             </footer>
